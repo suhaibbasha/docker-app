c1c04dc8df6b925c063d23c7ab1083f21b910ad9
suhaibbasha <suhaibbasha@icloud.com>
2025-02-18 20:23:54 +0100

    Spring setup

---
+++ src/test/java/io/training/suhaibbasha/SbDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.training.suhaibbasha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
